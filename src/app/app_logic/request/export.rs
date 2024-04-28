8da47a718fa167e8f0fe3a839e6a7a5f27205dba
julien-cpsn <julien.caposiena@gmail.com>
2024-04-28 15:51:35 +0200

    WIP

--- src/app/app_logic/request/export.rs
+++ src/app/app_logic/request/export.rs
@@ -48,14 +48,17 @@ impl App<'_> {
             }
         };
 
+        let method = request.method.to_string();
+
         let headers = self.key_value_vec_to_tuple_vec(&request.headers);
 
         match export_format {
+            /* ===== HTTP ===== */
             HTTP => {
                 /* URL & Query params */
 
                 output += &format!("{} {}{} HTTP/1.1",
-                    request.method.to_string(),
+                    method,
                     url.path(),
                     match url.query() {
                         None => String::new(),
@@ -143,29 +146,42 @@ impl App<'_> {
                         multipart_output
                     }
                     Form(form_data) => {
-                        let mut form_output = format!("\n--header 'Content-Type: {}' \\", &request.body.to_content_type());
 
                         let form = self.key_value_vec_to_tuple_vec(form_data);
+                        let mut form_str = String::new();
 
                         for (key, value) in form {
-
-                            form_output += &format!("\n--data-urlencode '{}={}' \\", key, value);
+                            form_str += &format!("\n--data-urlencode '{}={}' \\", key, value);
                         }
+
+                        let form_output = format!(
+                            "\nContent-Type: {}\nContent-Length: {}\n{}",
+                            &request.body.to_content_type(),
+                            form_str.len(),
+                            form_str
+                        );
 
                         form_output
                     }
                     Raw(body) | Json(body) | Xml(body) | Html(body) | Javascript(body) => {
-                        format!("\n--header 'Content-Type: {}' \\\n--data '{}' \\", &request.body.to_content_type(), body)
+                        format!(
+                            "\nContent-Type: {}\nContent-Length: {}\n\n{}",
+                            &request.body.to_content_type(),
+                            body.len(),
+                            body
+                        )
                     }
                 };
 
                 output
             },
+
+            /* ===== CURL ===== */
             Curl => {
                 /* URL & Query params */
 
                 output += &format!("curl --location --request {} '{}' \\",
-                   request.method.to_string(),
+                   method,
                    url
                 );
 
@@ -262,14 +278,146 @@ impl App<'_> {
                 
                 output
             },
+
+            /* ===== PHP Guzzle ===== */
             PhpGuzzle => {
+                output += "<?php\n$client = new GuzzleHttp\\Client();\n";
+
+                let mut headers_str = String::from("\n$headers = [");
+
+                /* Auth */
+
+                headers_str += &match &request.auth {
+                    Auth::NoAuth => String::new(),
+                    Auth::BasicAuth(username, password) => {
+                        let username = self.replace_env_keys_by_value(username);
+                        let password = self.replace_env_keys_by_value(password);
+
+                        format!("\n    'Authorization' => '{}',", encode_basic_auth(&username, &password))
+                    },
+                    Auth::BearerToken(bearer_token) => {
+                        let bearer_token = self.replace_env_keys_by_value(bearer_token);
+
+                        format!("\n    'Authorization' => 'Bearer {}',", bearer_token)
+                    }
+                };
+
+                /* Headers */
+
+                for (header, value) in &headers {
+                    headers_str += &format!("\n    '{}' => '{}',", header, value);
+                }
+
+                /* Proxy */
+
+                if request.settings.use_config_proxy {
+                    if let Some(proxy) = &self.config.proxy {
+                        let mut proxy_output = String::from("\n    'proxy' => [");
+
+                        if let Some(http_proxy) = &proxy.http_proxy {
+                            proxy_output += &format!("\n        'http' => '{}',", http_proxy);
+                        }
+
+                        if let Some(https_proxy) = &proxy.https_proxy {
+                            proxy_output += &format!("\n        'https' => '{}',", https_proxy);
+                        }
+
+                        output += &format!("{}\n    ],", proxy_output)
+                    }
+                }
+
+
+                headers_str += "\n];\n";
+                output += &headers_str;
+
+                /* Body */
+
+                let mut has_body = false;
+                let mut options_str: Option<String> = None;
+
+                match &request.body {
+                    NoBody => {},
+                    File(file_path) => {
+                        let file_path_with_env_values = self.replace_env_keys_by_value(file_path);
+
+                        let file_content = match get_file_content_with_name(PathBuf::from(file_path_with_env_values)) {
+                            Ok((content, _)) => content,
+                            Err(_) => {
+                                return String::from("Could not open file");
+                            }
+                        };
+
+                        has_body = true;
+                        output += &format!("\n$body = '{}';", String::from_utf8_lossy(&file_content));
+                    },
+                    Multipart(multipart) => {
+                        let mut multipart_output = String::new();
+
+                        // TODO
+                        for key_value in multipart {
+                            if !key_value.enabled {
+                                continue;
+                            }
+
+                            let key = self.replace_env_keys_by_value(&key_value.data.0);
+                            let mut value = self.replace_env_keys_by_value(&key_value.data.1);
+
+                            // If the value starts with !!, then it is supposed to be a file
+                            if value.starts_with("!!") {
+                                value = format!("@\"{}\"", &value[2..]);
+                            }
+                            else {
+                                value = format!("\"{}\"", &value);
+                            }
+
+                            multipart_output += &format!("\n--form '{}={}' \\", key, value);
+                        }
+
+                        multipart_output;
+                    }
+                    Form(form_data) => {
+                        let mut form_output = String::from("\n$options = [\n    'form_params' => [");
+
+                        let form = self.key_value_vec_to_tuple_vec(form_data);
+
+                        for (key, value) in form {
+                            form_output += &format!("\n    '{}' => '{}',", key, value);
+                        }
+
+                        form_output += "]];";
+
+                        options_str = Some(form_output);
+                    }
+                    Raw(body) | Json(body) | Xml(body) | Html(body) | Javascript(body) => {
+                        has_body = true;
+                        output += &format!("\n$body = '{body}';");
+                    }
+                };
+
+                output += &format!("\n$request = new Request('{method}', '{url}', $headers");
+
+                if has_body {
+                    output += ", $body";
+                };
+
+                output += ");";
+                output += "\n$res = $client->send($request";
+
+                output += match options_str {
+                    None => ");",
+                    Some(_) => ", $options);"
+                };
 
                 output
             },
+
+            /* ===== NodeJS Axios ===== */
             NodeJsAxios => {
 
                 output
             },
+
+            /* ===== Rust Reqwest ===== */
             RustReqwest => {
 
                 output
