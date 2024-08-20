c9097d2056cd91781758678813003f5024abea8a
anussel5559 <alex@redoxengine.com>
2024-08-19 21:45:03 -0500

    scripting-support: fine-tuning script_support

--- src/app/business_logic/request/script_support.rs
+++ src/app/business_logic/request/script_support.rs
@@ -3,14 +3,20 @@ use jsonwebtoken::Algorithm;
 
 use super::jwt::{generate_jwt_token, KeyFormat};
 
-pub fn generate_signed_jwt(_this: &JsValue, args: &[JsValue], _ctx: &mut Context) -> JsResult<JsValue> {
-    let claims: &str =  &args.get(0).unwrap().as_string().unwrap().to_std_string().unwrap();
-    let private_key_string: &str = &args.get(1).unwrap().as_string().unwrap().to_std_string().unwrap();
-    let kid = args.get(2).unwrap().as_string().unwrap().to_std_string().unwrap();
-    let alg: &str = &args.get(3).unwrap().as_string().unwrap().to_std_string().unwrap();
-    let raw_key_format: &str = &args.get(4).unwrap().as_string().unwrap().to_std_string().unwrap();
+fn build_arg_extractor<'a>(args: &'a [JsValue]) -> Box<dyn Fn(usize) -> String + 'a> {
+    Box::new(move |index| args.get(index).unwrap().as_string().unwrap().to_std_string().unwrap())
+}
 
-    let rust_alg = match alg {
+pub fn generate_signed_jwt(_this: &JsValue, args: &[JsValue], _ctx: &mut Context) -> JsResult<JsValue> {
+    let arg_extractor = build_arg_extractor(args);
+
+    let claims = arg_extractor(0);
+    let private_key_string = arg_extractor(1);
+    let kid = arg_extractor(2);
+    let alg = arg_extractor(3);
+    let raw_key_format = arg_extractor(4);
+
+    let rust_alg = match alg.as_str() {
         "HS256" => Some(Algorithm::HS256),
         "HS384" => Some(Algorithm::HS384),
         "HS512" => Some(Algorithm::HS512),
@@ -25,7 +31,7 @@ pub fn generate_signed_jwt(_this: &JsValue, args: &[JsValue], _ctx: &mut Context
         _ => None
     };
 
-    let key_format = match raw_key_format {
+    let key_format = match raw_key_format.as_str() {
         "pem" => Some(KeyFormat::PEM),
         "der" => Some(KeyFormat::DER),
         "b64" => Some(KeyFormat::B64),
@@ -33,7 +39,7 @@ pub fn generate_signed_jwt(_this: &JsValue, args: &[JsValue], _ctx: &mut Context
         _ => None
     };
 
-    let token = generate_jwt_token(claims, private_key_string, Some(kid), rust_alg, key_format);
+    let token = generate_jwt_token(&claims, &private_key_string, Some(kid), rust_alg, key_format);
     match token {
         Ok(token) => Ok(JsValue::String(JsString::from(token))),
         Err(e) => Err(JsError::from_opaque(JsValue::String(JsString::from(format!("Error in generate_signed_jwt: {}", e.to_string())))))
