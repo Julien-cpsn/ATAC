0ed10e26d383f289eb63d9a8d22639aa5efbc4ff
Julien C. <64107022+Julien-cpsn@users.noreply.github.com>
2025-09-08 21:33:04 +0200

    Update utils.rs
    Used self instead of clipboard

--- src/tui/tui_logic/request/utils.rs
+++ src/tui/tui_logic/request/utils.rs
@@ -13,21 +13,19 @@ impl App<'_> {
         let local_selected_request = self.get_selected_request_as_local();
         let selected_request = local_selected_request.read();
 
-        let clipboard = &mut self.clipboard;
-
         match self.request_result_tab {
             RequestResultTabs::Body => match &selected_request.response.content {
                 None => {}
                 Some(content) => match content {
                     ResponseContent::Body(body) => {
-                        clipboard.set_text(body).expect("Could not copy response content to clipboard");
+                        self.clipboard.set_text(body).expect("Could not copy response content to clipboard");
                     }
                     ResponseContent::Image(image_response) => match &image_response.image {
                         None => {}
                         Some(image) => {
                             let rgba_image = image.to_rgba8();
 
-                            clipboard
+                            self.clipboard
                                 .set_image(ImageData {
                                     width: rgba_image.width() as usize,
                                     height: rgba_image.height() as usize,
@@ -40,7 +38,7 @@ impl App<'_> {
             }
             RequestResultTabs::Cookies => match &selected_request.response.cookies {
                 None => {}
-                Some(cookies) => clipboard.set_text(cookies).expect("Could not copy cookies to clipboard")
+                Some(cookies) => self.clipboard.set_text(cookies).expect("Could not copy cookies to clipboard")
             }
             RequestResultTabs::Headers => {
                 let headers_string: String = selected_request.response.headers
@@ -48,15 +46,15 @@ impl App<'_> {
                     .map(|(header, value)| format!("{}: {}\n", header, value))
                     .collect();
 
-                clipboard.set_text(headers_string).expect("Could not copy headers to clipboard")
+                self.clipboard.set_text(headers_string).expect("Could not copy headers to clipboard")
             }
             RequestResultTabs::Console => {
 
                 match (&selected_request.console_output.pre_request_output, &selected_request.console_output.post_request_output) {
                     (None, None) => {}
-                    (Some(pre_request_console_output), None) => clipboard.set_text(pre_request_console_output).expect("Could not copy console output to clipboard"),
-                    (None, Some(post_request_console_output)) => clipboard.set_text(post_request_console_output).expect("Could not copy console output to clipboard"),
-                    (Some(pre_request_console_output), Some(post_request_console_output)) => clipboard
+                    (Some(pre_request_console_output), None) => self.clipboard.set_text(pre_request_console_output).expect("Could not copy console output to clipboard"),
+                    (None, Some(post_request_console_output)) => self.clipboard.set_text(post_request_console_output).expect("Could not copy console output to clipboard"),
+                    (Some(pre_request_console_output), Some(post_request_console_output)) => self.clipboard
                         .set_text(format!("{}\n{}", pre_request_console_output, post_request_console_output))
                         .expect("Could not copy console output to clipboard"),
                 }
